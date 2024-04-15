701ada87370f2acc76a6b13b943cdf2af1eb1ad8
OrganistaF <0243794@up.edu.mx>
2024-04-15 23:07:26 +0200

    IMC Calculator.

--- app/src/main/java/edu/utad/md_imc/MainActivity.kt
+++ app/src/main/java/edu/utad/md_imc/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
 import android.widget.RadioButton
-import androidx.core.view.isNotEmpty
 import com.google.android.material.snackbar.Snackbar
 import edu.utad.md_imc.databinding.ActivityMainBinding
 
